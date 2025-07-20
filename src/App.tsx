1d7f5b2099c0969555526fec051c96339bd0ff5c
AestheticShelby33 <altonmartin3333@gmail.com>
2025-07-20 21:03:16 +0530

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import Feed from './components/Feed';
-import React from 'react';
 import {Route, Routes} from 'react-router-dom';
-import GoogleSignInClone from './components/GoogleSignIn';
 import GoogleSignUp from './components/GoogleSignUp';
 import GoogleSignIn from './components/GoogleSignIn';
 
